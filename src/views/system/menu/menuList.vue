4203d84fee77e7c5be28641c6ede9a666fd068db
meixiaoke <2422551515@qq.com>
2022-09-14 16:40:43 +0800

    封装icon组件

--- src/views/system/menu/menuList.vue
+++ src/views/system/menu/menuList.vue
@@ -121,28 +121,7 @@
             <el-input v-model="menu.code"></el-input>
           </el-form-item>
           <el-form-item size="small" label="菜单图标">
-            <div class="chooseIcons">
-              <el-popover placement="bottom" width="450" trigger="click">
-            <span
-              slot="reference"
-              style="
-              display: inline-block;
-              width: 200px;
-              height: 33px;
-              line-height: 33px;"
-            >
-            <i :class="userChooseIcon"></i>
-            {{ userChooseIcon }}
-            </span>
-                <div class="iconList">
-                  <i v-for="item in iconList" :key="item"
-                     :class="item"
-                     @click="setIcon(item)"
-                     style="font-size: 20px"
-                  ></i>
-                </div>
-              </el-popover>
-            </div>
+            <my-icon @selecticon="setIcon" ref="child"></my-icon>
           </el-form-item>
           <el-form-item size="small" label="菜单序号">
             <el-input v-model="menu.orderNum"></el-input>
@@ -199,11 +178,14 @@ import menu from '@/api/menu'
 import SystemDialog from '@/components/system/SystemDialog'
 //导入自定义的icon图标库
 import { elementIcons } from '@/utils/icons'
+//导入自定义图标组件
+import MyIcon from '@/components/system/MyIcon.vue'
 
 export default {
   name: 'menuList',
   components: {
-    SystemDialog
+    SystemDialog,
+    MyIcon
   },
   data() {
     return {
@@ -259,7 +241,6 @@ export default {
   methods: {
     //给icon绑定的点击事件
     setIcon(icon) {
-      this.userChooseIcon = icon //将i的样式设为选中的样式el-icon-xxx
       this.menu.icon = icon
     },
 
@@ -343,7 +324,9 @@ export default {
       this.menu.type = 0
       this.menuDialog.title = '新增菜单'
       this.menuDialog.visible = true
-      this.userChooseIcon = ''//清空菜单图标
+      this.$nextTick(() => {
+        this.$refs['child'].userChooseIcon = ''//清空菜单图标
+      })
     }
     ,
 
@@ -383,51 +366,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.iconList {
-  width: 400px;
-  height: 300px;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  display: flex;
-  justify-content: space-around;
-
-  flex-wrap: wrap;
-
-  i {
-    display: inline-block;
-    width: 60px;
-    height: 45px;
-    color: #000000;
-    font-size: 20px;
-    border: 1px solid #e6e6e6;
-    border-radius: 4px;
-    cursor: pointer;
-    text-align: center;
-    line-height: 45px;
-    margin: 5px;
-
-    &:hover {
-      color: orange;
-      border-color: orange;
-    }
-  }
-}
-
-.chooseIcons {
-  width: 175px;
-  background-color: #FFFFFF;
-  background-image: none;
-  border-radius: 4px;
-  border: 1px solid #DCDFE6;
-  box-sizing: border-box;
-  color: #606266;
-  display: inline-block;
-  font-size: inherit;
-  height: 33px;
-  line-height: 25px;
-  outline: none;
-  padding: 0 15px;
-  transition: border-color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
-}
 
 </style>
